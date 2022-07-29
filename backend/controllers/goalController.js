89e4be1f90af1a6dd9ff79afb79771d3f20ae5d8
Mansor Almossa <malmossa@gmail.com>
2022-07-29 10:37:09 -0700

    completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,6 +5,10 @@ const getGoals = (req, res) => {
 
 // set new goal
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ message: "Set goals" });
 };
 
