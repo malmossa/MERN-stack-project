50d71d1df665389ba010a5f16a24936718e3862d
Mansor Almossa <malmossa@gmail.com>
2022-07-31 08:33:45 -0700

    completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,26 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
 // get all goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // set new goal
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Set goals" });
-};
+});
 
 // update goal
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // delete goal
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
